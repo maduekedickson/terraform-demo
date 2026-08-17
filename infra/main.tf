d560ea6608bef660977b9f011b4576f133370954
Madueke Dickson <maduekedickson@gmail.com>
2026-08-17 18:51:47 +0100

    another-region

--- infra/main.tf
+++ infra/main.tf
@@ -1,6 +1,6 @@
 # Create a new security group
-resource "aws_security_group" "terraform_sec_group" {
-  name        = "terraform-sec-group"
+resource "aws_security_group" "terraform_secu_group" {
+  name        = "terraform-secu-group"
   description = "Allow SSH and HTTP"
   vpc_id      = data.aws_vpc.default.id
 
@@ -37,9 +37,9 @@ data "aws_vpc" "default" {
 resource "aws_instance" "my_ec2" {
   ami           = "ami-0aba19e56f3eaec05"
   instance_type = "t3.micro"
-  key_name      = "Red-Login" # Use the key-pair name without .pem
+  key_name      = "data-engr" # Use the key-pair name without .pem
 
-  vpc_security_group_ids = [aws_security_group.terraform_sec_group.id]
+  vpc_security_group_ids = [aws_security_group.terraform_secu_group.id]
 
   user_data = <<-EOF
               #!/bin/bash
