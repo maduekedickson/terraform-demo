1252ac51f98103470fc101ae46826b22008f9c60
Madueke Dickson <maduekedickson@gmail.com>
2026-08-17 18:54:30 +0100

    another-AMI

--- infra/main.tf
+++ infra/main.tf
@@ -35,7 +35,7 @@ data "aws_vpc" "default" {
 
 # Launch EC2 instance
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-0aba19e56f3eaec05"
+  ami           = "ami-0b6d9d3d33ba97d99"
   instance_type = "t3.micro"
   key_name      = "data-engr" # Use the key-pair name without .pem
 
