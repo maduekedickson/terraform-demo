0e6396a7e53153220ce1995f73c1382bc09ec737
Madueke Dickson <maduekedickson@gmail.com>
2026-08-17 17:40:32 +0100

    key-pair-changed

--- infra/main.tf
+++ infra/main.tf
@@ -37,7 +37,7 @@ data "aws_vpc" "default" {
 resource "aws_instance" "my_ec2" {
   ami           = "ami-0aba19e56f3eaec05"
   instance_type = "t3.micro"
-  key_name      = "data-engr" # Use the key-pair name without .pem
+  key_name      = "Red-Login" # Use the key-pair name without .pem
 
   vpc_security_group_ids = [aws_security_group.terra_sec_group.id]
 
