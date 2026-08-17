40f05cb83837f67a6cd1ce3a6eee88adb7041f6b
Madueke Dickson <maduekedickson@gmail.com>
2026-08-17 17:08:32 +0100

    changes made

--- infra/main.tf
+++ infra/main.tf
@@ -37,7 +37,7 @@ data "aws_vpc" "default" {
 resource "aws_instance" "my_ec2" {
   ami           = "ami-0aba19e56f3eaec05"
   instance_type = "t3.micro"
-  key_name      = "my-logging-in" # Use the key-pair name without .pem
+  key_name      = "data-engr" # Use the key-pair name without .pem
 
   vpc_security_group_ids = [aws_security_group.new_sec_group.id]
 
